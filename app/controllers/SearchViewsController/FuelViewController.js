c27eae924f861b17e37f5ac30350875d7bcd0dbf
gsuslol <mad198@me.com>
2012-06-25 20:26:56 +0200

    -

--- app/controllers/SearchViewsController/FuelViewController.js
+++ app/controllers/SearchViewsController/FuelViewController.js
@@ -34,7 +34,7 @@ MobileGas.FuelViewController = M.Controller.extend({
         var selectionList = M.ViewManager.getView('searchFuelView', 'fuelSelection');
         var selection = selectionList.getSelection(YES);
         if (selection.length == 0) {
-            return '1;2;3;4;12';//alle kraftstoffe einbauen
+            return '1;2;3;4;5;6;7;8;9;10;11;12;13;14;15;16;17;18;19;20';//alle kraftstoffe einbauen
         } else {
             var string = '';
             for (var i in selection) {
